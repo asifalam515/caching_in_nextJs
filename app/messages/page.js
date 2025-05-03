53772dcde94923929a0b52d823c4707efe54c240
asifalam515 <asibulalam515@gmail.com>
2025-05-03 23:22:02 +0600

    caching added

--- app/messages/page.js
+++ app/messages/page.js
@@ -1,11 +1,13 @@
-import Messages from '@/components/messages';
+import Messages from "@/components/messages";
 
 export default async function MessagesPage() {
-  const response = await fetch('http://localhost:8080/messages', {
-    headers: {
-      'X-ID': 'page',
-    },
+  const response = await fetch("http://localhost:8080/messages", {
+    // cache: "force-cache", aggresive caching . catch everything you find
+    cache: "no-store", //this place the data should not be cached
   });
+  // headers: {
+  //   "X-ID": "page",
+  // },
   const messages = await response.json();
 
   if (!messages || messages.length === 0) {
